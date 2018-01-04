b3d332a6aac0ea132a445d8998a7984df03dc84c
Dennis Stengele <dstengele@seibert-media.net>
2018-01-05 00:10:11 +0100

    Remove onetime secret (don't worry, I've changed it :P)

--- zsh/.zsh/99-secret.zsh
+++ zsh/.zsh/99-secret.zsh
@@ -1,1 +1,1 @@
-alias onetime='onetime -H "https://secret.seibert-media.net/api" -c "dstengele@seibert-media.net" -k "677e7651a947299bf56d526dc170ce538391cb65"'
+alias onetime='onetime -H "https://secret.seibert-media.net/api" -c "dstengele@seibert-media.net" -k $(<~/.onetime_token)'

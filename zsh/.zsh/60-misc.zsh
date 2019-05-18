050d57eb28b6ac019f8e4e0b7cda3806e93482c6
Dennis Stengele <dennis@stengele.me>
2019-05-18 15:47:13 +0200

    Remove Mac specific settings from dotfiles

--- zsh/.zsh/60-misc.zsh
+++ zsh/.zsh/60-misc.zsh
@@ -5,16 +5,10 @@
 # Options
 setopt AUTO_CD
 
-# SSH-Agent
-is_mac && export SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh
-
 # fasd
 eval "$(fasd --init auto)"
 
 alias fuck='$(thefuck $(fc -ln -1))'
-
-is_mac && source "`brew --prefix`/etc/grc.bashrc"
-fpath=(/usr/local/share/zsh-completions $fpath)
 
 # Direnv
 eval "$(direnv hook zsh)"

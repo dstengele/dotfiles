14ab88203ea4b731141a289b7ec0d90803be2d75
Dennis Stengele <Dennis.Stengele.extern@dvag.com>
2021-06-22 10:41:43 +0200

    Respect different paths for system installed zsh-syntax-highlighting

--- zsh/.zsh/99-plugins.zsh
+++ zsh/.zsh/99-plugins.zsh
@@ -1,1 +1,5 @@
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+if [ -f /etc/debian_version ]; then
+    source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+else
+    source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+fi

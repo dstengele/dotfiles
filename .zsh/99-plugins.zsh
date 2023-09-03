71facfe61c797f19bb75568b98557a5647144e9f
Dennis Stengele <dstengele@seibert-media.net>
2023-09-03 21:27:58 +0200

    Convert Plugins to submodules with nativ package functionality in vim 8

--- .zsh/99-plugins.zsh
+++ .zsh/99-plugins.zsh
@@ -4,6 +4,8 @@ fi
 
 if [ -f /etc/debian_version ]; then
     source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+elif [ -f /run/current-system/sw/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]; then
+    source /run/current-system/sw/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 else
     source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 fi
